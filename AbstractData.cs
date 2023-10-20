f844e807aa030c6e41ae8d263b35cfe038aa0911
Zack Spring Chief <jzacksc@gmail.com>
2023-10-19 18:16:03 -0600

    data storage is like a drawer

--- AbstractData.cs
+++ AbstractData.cs
@@ -1,4 +1,24 @@
 //What is abstract data type -- a "Class";
 //example is CRUD - create, read, update, delete;
 //Reperesention and Operation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace datastructure
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public int Exp { get; set; }
+        public int Level { get; set; }
+        public int DexNum { get; set; }
+
+        public bool IsPokemon()
+        {
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,3 @@
-﻿namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("enter your name");
-            string name = Console.ReadLine();
-            Console.WriteLine("hello, " + name);
-        }
-    }
-}
+﻿
+
+Pokemon charmander = new Pokemon();
